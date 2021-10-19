97b99e2b2bc8e77778ff6cadc38919eafed305c0
Sagar Kumar <74974420+sagarkumar3105@users.noreply.github.com>
2021-10-19 16:37:38 +0530

    Add files via upload

---
+++ build_final.bat
@@ -0,0 +1,94 @@
+@echo off
+
+set loc=%cd% 
+cd %loc%
+
+powershell -Command "invoke-WebRequest https://github.com/skeeto/w64devkit/releases/download/v1.10.0/w64devkit-1.10.0.zip -Outfile w64devkit_comp.zip"
+powershell -Command expand-Archive -Path "w64devkit_comp.zip" -DestinationPath .
+setx PATH=%loc%\w64devkit\bin
+
+::This line onwards will be added to the build.batch and source directory will be %loc% we need to come out and chaneg dir to 'installers'
+mkdir installers
+cd installers
+echo Make sure you are connected to a network 
+pause
+echo Downlaoding Ruby 3.0.2-1 ...
+powershell -Command "invoke-WebRequest https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.0.2-1/rubyinstaller-devkit-3.0.2-1-x64.exe -Outfile ruby_installer.exe"
+echo Ruby Downloaded
+pause
+
+::Downloading PYTHON
+echo Downlaoding python 3.9.7 ...
+powershell -Command "invoke-WebRequest https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe -Outfile python_installer.exe"
+echo Python Downloaded
+pause
+
+::Downloading DOTNET
+echo Downlaoding .NET 5.0 Runtime...
+powershell -Command "invoke-WebRequest https://dotnet.microsoft.com/download/dotnet/thank-you/runtime-5.0.10-windows-x64-installer -Outfile dotnet_installer.exe"
+echo DOTNET Downloaded
+pause
+
+::Downloading NODE
+echo Downlaoding NOde.js...
+powershell -Command "invoke-WebRequest https://nodejs.org/dist/v14.18.0/node-v14.18.0-x64.msi -Outfile node_installer.msi"
+echo DOTNET Downloaded
+pause
+
+::come out of intallations and create and go to dep
+cd..
+mkdir dep
+cd installers
+
+::Installation for Ruby
+echo Going to install all dependencies via GUI, don't close this window.
+echo Installing Ruby...
+mkdir Ruby
+
+::we can also add verysilent- for no UI and Silent-for lest UI 
+ruby_installer/DIR="%loc%\dep\Ruby" /VERYSILENT
+echo Ruby Installed
+setx PATH=%PATH%;%loc%\dep\Ruby\bin
+pause
+
+::For Python
+echo Installing python...
+mkdir Python
+python_installer.exe/passive TargetDir="%loc%\dep\Python" PrependPath=1
+echo Python installed
+pause
+
+::for DOTNET - still pending ui and targetdir
+echo Installing dotnet..
+mkdir Dotnet
+dotnet_installer.exe
+echo dotnet installed
+
+
+echo Installing dotnet..
+mkdir Dotnet
+::can be quite or passive
+msiexec.exe /i node_installer.msi INSTALLDIR="%loc%\dep\Dotnet" /passive
+echo dotnet installed
+
+echo All Dependencies Installed
+
+
+git clone https://github.com/metacall/core.git
+
+cd core
+mkdir Build
+
+cd Build
+
+::CMAKE must be installed in the system
+cmake -Wno-dev -DCMAKE_BUILD_TYPE=Release -DOPTION_BUILD_SECURITY=OFF -DOPTION_FORK_SAFE=Off -DOPTION_BUILD_LOADERS_PY=ON -DPython_ROOT_DIR=C:/Users/Sagar Kumar/Documents/For_Meta_call/dep/Python -DOPTION_BUILD_LOADERS_NODE=ON -DOPTION_BUILD_LOADERS_CS=ON -DOPTION_BUILD_LOADERS_RB=ON -DOPTION_BUILD_LOADERS_TS=ON -G "MinGW Makefiles" ..
+cmake --build .
+
+::Setting up vcpkg
+::git clone https://github.com/Microsoft/vcpkg.git
+::building vcpkg and using disableMetrics to avoid data share
+::.\vcpkg\bootstrap-vcpkg.bat -disableMetrics  
+
+echo Your things are ready 
+pause >nul
