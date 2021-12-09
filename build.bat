ad639027c6f87937dbd34a9fe5a3a57fe089f491
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2021-12-09 10:04:06 +0100

    Removed progress with w64devkit.

--- build.bat
+++ build.bat
@@ -11,7 +11,7 @@ echo Checking Compiler and Build System
 
 rem Devkit is also required for building Ruby
 powershell -Command "invoke-WebRequest https://github.com/skeeto/w64devkit/releases/download/v1.10.0/w64devkit-1.10.0.zip -Outfile w64devkit.zip"
-powershell -Command expand-Archive -Path "w64devkit.zip" -DestinationPath .
+powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "w64devkit.zip" -DestinationPath .
 set PATH=%PATH%;%loc%\w64devkit\bin
 del w64devkit.zip
 
