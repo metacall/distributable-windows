9bfa99e39a602f3186d14572c67ed600b54624df
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2021-12-14 22:32:21 +0200

    Remove unnecessary build from NodeJS.

--- build.bat
+++ build.bat
@@ -87,9 +87,7 @@ rmdir /S /Q %loc%\dependencies\node-v14.18.2
 rem Build NodeJS (DLL)
 cmake -E tar xzf node_src.tar.gz || goto :error
 cd %loc%\dependencies\node-v14.18.2 || goto :error
-call .\vcbuild.bat || goto :error
 python %loc%\dependencies\NodeJSGYPPatch.py %loc%\dependencies\node-v14.18.2\node.gyp || goto :error
-rem Next command will fail but it will produce a valid node.dll
 call .\vcbuild.bat dll
 mkdir %loc%\runtimes\nodejs\lib || goto :error
 move %loc%\dependencies\node-v14.18.2\out\Release\libnode.lib %loc%\runtimes\nodejs\lib\libnode.lib || goto :error
