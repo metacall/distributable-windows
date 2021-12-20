add416a925c3a74221bc9512ba9f613bda54ef0b
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2021-12-20 15:08:26 +0100

    Add quotes to all the paths in the build script, remove the deletion of the executables, pip3 requires python for example. Move patch of C# config into build step. Add script for running package managers from metacall command.

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ rem Use all the cores when building
 set CL=/MP
 
 rem Output directory
-set dest=%cd%
+set "dest=%cd%"
 
 echo Checking Compiler and Build System
 
@@ -14,7 +14,7 @@ if %errorlevel% EQU 0 (goto skip_build_system)
 rem Install CMake if not found
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1-windows-x86_64.zip', './cmake.zip')" || goto :error
 powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "cmake.zip" -DestinationPath . || goto :error
-set PATH=%PATH%;%dest%\cmake-3.22.1-windows-x86_64\bin
+set "PATH=%PATH%;%dest%\cmake-3.22.1-windows-x86_64\bin"
 del cmake.zip
 
 :skip_build_system
@@ -27,13 +27,13 @@ set loc=%cd%
 rem Install NASM for NodeJS
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win64/nasm-2.15.05-win64.zip', './nasm.zip')" || goto :error
 powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "nasm.zip" -DestinationPath . || goto :error
-set PATH=%PATH%;%loc%\nasm-2.15.05;%loc%\nasm-2.15.05\rdoff
+set "PATH=%PATH%;%loc%\nasm-2.15.05;%loc%\nasm-2.15.05\rdoff"
 del nasm.zip
 
 echo Downloading Dependencies
 
-mkdir %loc%\dependencies
-cd %loc%\dependencies
+mkdir "%loc%\dependencies"
+cd "%loc%\dependencies"
 
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/MSP-Greg/ruby-loco/releases/download/ruby-master/ruby-mswin.7z', './ruby-mswin.7z')" || goto :error
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe', './python_installer.exe')" || goto :error
@@ -45,19 +45,19 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.github
 
 echo Installing Runtimes
 
-mkdir %loc%\runtimes
-mkdir %loc%\runtimes\ruby
-mkdir %loc%\runtimes\python
-mkdir %loc%\runtimes\dotnet
-mkdir %loc%\runtimes\nodejs
+mkdir "%loc%\runtimes"
+mkdir "%loc%\runtimes\ruby"
+mkdir "%loc%\runtimes\python"
+mkdir "%loc%\runtimes\dotnet"
+mkdir "%loc%\runtimes\nodejs"
 
-cd %loc%\dependencies
+cd "%loc%\dependencies"
 
 rem Install Ruby
 set PATH=%PATH%;%programfiles%\7-Zip\
-7z x %loc%\dependencies\ruby-mswin.7z || goto :error
-robocopy /move /e %loc%\dependencies\ruby-mswin %loc%\runtimes\ruby /NFL /NDL /NJH /NJS /NC /NS /NP
-set PATH=%PATH%;%loc%\runtimes\ruby\bin
+7z x "%loc%\dependencies\ruby-mswin.7z" || goto :error
+robocopy /move /e "%loc%\dependencies\ruby-mswin" %loc%\runtimes\ruby /NFL /NDL /NJH /NJS /NC /NS /NP
+set "PATH=%PATH%;%loc%\runtimes\ruby\bin"
 
 rem Install Python
 where /Q python
@@ -69,33 +69,33 @@ python_installer.exe /uninstall || goto :error
 :skip_uninstall_python
 
 python_installer.exe /quiet TargetDir="%loc%\runtimes\python" PrependPath=1 CompileAll=1 || goto :error
-set PATH=%PATH%;%loc%\runtimes\python
+set "PATH=%PATH%;%loc%\runtimes\python"
 
 rem Install DotNet
-powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "dotnet_sdk.zip" -DestinationPath %loc%\runtimes\dotnet || goto :error
-git clone --branch v5.0.12 --depth 1 --single-branch https://github.com/dotnet/runtime.git %loc%\runtimes\dotnet\runtime
-mkdir %loc%\runtimes\dotnet\include
-robocopy /move /e %loc%\runtimes\dotnet\runtime\src\coreclr\src\pal %loc%\runtimes\dotnet\include\pal /NFL /NDL /NJH /NJS /NC /NS /NP
-robocopy /move /e %loc%\runtimes\dotnet\runtime\src\coreclr\src\inc %loc%\runtimes\dotnet\include\inc /NFL /NDL /NJH /NJS /NC /NS /NP
-rmdir /S /Q %loc%\runtimes\dotnet\runtime
-set PATH=%PATH%;%loc%\runtimes\dotnet
+powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "dotnet_sdk.zip" -DestinationPath "%loc%\runtimes\dotnet" || goto :error
+git clone --branch v5.0.12 --depth 1 --single-branch https://github.com/dotnet/runtime.git "%loc%\runtimes\dotnet\runtime"
+mkdir "%loc%\runtimes\dotnet\include"
+robocopy /move /e "%loc%\runtimes\dotnet\runtime\src\coreclr\src\pal" "%loc%\runtimes\dotnet\include\pal" /NFL /NDL /NJH /NJS /NC /NS /NP
+robocopy /move /e "%loc%\runtimes\dotnet\runtime\src\coreclr\src\inc" "%loc%\runtimes\dotnet\include\inc" /NFL /NDL /NJH /NJS /NC /NS /NP
+rmdir /S /Q "%loc%\runtimes\dotnet\runtime"
+set "PATH=%PATH%;%loc%\runtimes\dotnet"
 
 rem Install NodeJS
-powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "node.zip" -DestinationPath %loc%\runtimes\nodejs || goto :error
-robocopy /move /e %loc%\runtimes\nodejs\node-v14.18.2-win-x64 %loc%\runtimes\nodejs /NFL /NDL /NJH /NJS /NC /NS /NP
-rmdir %loc%\runtimes\nodejs\node-v14.18.2-win-x64
-set PATH=%PATH%;%loc%\runtimes\nodejs
+powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "node.zip" -DestinationPath "%loc%\runtimes\nodejs" || goto :error
+robocopy /move /e "%loc%\runtimes\nodejs\node-v14.18.2-win-x64" "%loc%\runtimes\nodejs" /NFL /NDL /NJH /NJS /NC /NS /NP
+rmdir "%loc%\runtimes\nodejs\node-v14.18.2-win-x64"
+set "PATH=%PATH%;%loc%\runtimes\nodejs"
 
 rem Install NodeJS Headers
 cmake -E tar xzf node_headers.tar.gz || goto :error
-cd %loc%\dependencies\node-v14.18.2 || goto :error
+cd "%loc%\dependencies\node-v14.18.2" || goto :error
 mkdir %loc%\runtimes\nodejs\include
-robocopy /move /e %loc%\dependencies\node-v14.18.2\include %loc%\runtimes\nodejs\include /NFL /NDL /NJH /NJS /NC /NS /NP
+robocopy /move /e "%loc%\dependencies\node-v14.18.2\include" "%loc%\runtimes\nodejs\include" /NFL /NDL /NJH /NJS /NC /NS /NP
 cd %loc%\dependencies
-rmdir /S /Q %loc%\dependencies\node-v14.18.2
+rmdir /S /Q "%loc%\dependencies\node-v14.18.2"
 
 rem Install NodeJS DLL
-powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "node_dll.zip" -DestinationPath %loc%\runtimes\nodejs\lib || goto :error
+powershell -Command "$global:ProgressPreference = 'SilentlyContinue'; Expand-Archive" -Path "node_dll.zip" -DestinationPath "%loc%\runtimes\nodejs\lib" || goto :error
 
 echo Building MetaCall
 
@@ -106,52 +106,52 @@ git clone --depth 1 https://github.com/metacall/core.git || goto :error
 set "escaped_loc=%loc:\=/%"
 
 rem Patch for FindRuby.cmake
-echo set(Ruby_VERSION 3.1.0)> %loc%\core\cmake\FindRuby.cmake
-echo set(Ruby_ROOT_DIR "%escaped_loc%/runtimes/ruby")>> %loc%\core\cmake\FindRuby.cmake
-echo set(Ruby_EXECUTABLE "%escaped_loc%/runtimes/ruby/bin/ruby.exe")>> %loc%\core\cmake\FindRuby.cmake
-echo set(Ruby_INCLUDE_DIRS "%escaped_loc%/runtimes/ruby/include/ruby-3.1.0;%escaped_loc%/runtimes/ruby/include/ruby-3.1.0/x64-mswin64_140")>> %loc%\core\cmake\FindRuby.cmake
-echo set(Ruby_LIBRARY "%escaped_loc%/runtimes/ruby/lib/x64-vcruntime140-ruby310.lib")>> %loc%\core\cmake\FindRuby.cmake
-echo include(FindPackageHandleStandardArgs)>> %loc%\core\cmake\FindRuby.cmake
-echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(Ruby REQUIRED_VARS Ruby_EXECUTABLE Ruby_LIBRARY Ruby_INCLUDE_DIRS VERSION_VAR Ruby_VERSION)>> %loc%\core\cmake\FindRuby.cmake
-echo mark_as_advanced(Ruby_EXECUTABLE Ruby_LIBRARY Ruby_INCLUDE_DIRS)>> %loc%\core\cmake\FindRuby.cmake
+echo set(Ruby_VERSION 3.1.0)> "%loc%\core\cmake\FindRuby.cmake"
+echo set(Ruby_ROOT_DIR "%escaped_loc%/runtimes/ruby")>> "%loc%\core\cmake\FindRuby.cmake"
+echo set(Ruby_EXECUTABLE "%escaped_loc%/runtimes/ruby/bin/ruby.exe")>> "%loc%\core\cmake\FindRuby.cmake"
+echo set(Ruby_INCLUDE_DIRS "%escaped_loc%/runtimes/ruby/include/ruby-3.1.0;%escaped_loc%/runtimes/ruby/include/ruby-3.1.0/x64-mswin64_140")>> "%loc%\core\cmake\FindRuby.cmake"
+echo set(Ruby_LIBRARY "%escaped_loc%/runtimes/ruby/lib/x64-vcruntime140-ruby310.lib")>> "%loc%\core\cmake\FindRuby.cmake"
+echo include(FindPackageHandleStandardArgs)>> "%loc%\core\cmake\FindRuby.cmake"
+echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(Ruby REQUIRED_VARS Ruby_EXECUTABLE Ruby_LIBRARY Ruby_INCLUDE_DIRS VERSION_VAR Ruby_VERSION)>> "%loc%\core\cmake\FindRuby.cmake"
+echo mark_as_advanced(Ruby_EXECUTABLE Ruby_LIBRARY Ruby_INCLUDE_DIRS)>> "%loc%\core\cmake\FindRuby.cmake"
 
 rem Patch for FindPython.cmake
-echo set(Python_VERSION 3.9.7)> %loc%\core\cmake\FindPython.cmake
-echo set(Python_ROOT_DIR "%escaped_loc%/runtimes/python")>> %loc%\core\cmake\FindPython.cmake
-echo set(Python_EXECUTABLE "%escaped_loc%/runtimes/python/python.exe")>> %loc%\core\cmake\FindPython.cmake
-echo set(Python_INCLUDE_DIRS "%escaped_loc%/runtimes/python/include")>> %loc%\core\cmake\FindPython.cmake
-echo set(Python_LIBRARIES "%escaped_loc%/runtimes/python/libs/python39.lib")>> %loc%\core\cmake\FindPython.cmake
-echo include(FindPackageHandleStandardArgs)>> %loc%\core\cmake\FindPython.cmake
-echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(Python REQUIRED_VARS Python_EXECUTABLE Python_LIBRARIES Python_INCLUDE_DIRS VERSION_VAR Python_VERSION)>> %loc%\core\cmake\FindPython.cmake
-echo mark_as_advanced(Python_EXECUTABLE Python_LIBRARIES Python_INCLUDE_DIRS)>> %loc%\core\cmake\FindPython.cmake
+echo set(Python_VERSION 3.9.7)> "%loc%\core\cmake\FindPython.cmake"
+echo set(Python_ROOT_DIR "%escaped_loc%/runtimes/python")>> "%loc%\core\cmake\FindPython.cmake"
+echo set(Python_EXECUTABLE "%escaped_loc%/runtimes/python/python.exe")>> "%loc%\core\cmake\FindPython.cmake"
+echo set(Python_INCLUDE_DIRS "%escaped_loc%/runtimes/python/include")>> "%loc%\core\cmake\FindPython.cmake"
+echo set(Python_LIBRARIES "%escaped_loc%/runtimes/python/libs/python39.lib")>> "%loc%\core\cmake\FindPython.cmake"
+echo include(FindPackageHandleStandardArgs)>> "%loc%\core\cmake\FindPython.cmake"
+echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(Python REQUIRED_VARS Python_EXECUTABLE Python_LIBRARIES Python_INCLUDE_DIRS VERSION_VAR Python_VERSION)>> "%loc%\core\cmake\FindPython.cmake"
+echo mark_as_advanced(Python_EXECUTABLE Python_LIBRARIES Python_INCLUDE_DIRS)>> "%loc%\core\cmake\FindPython.cmake"
 
 rem Patch for FindCoreCLR.cmake
-echo set(CoreCLR_VERSION 5.0.12)> %loc%\core\cmake\FindCoreCLR.cmake
-echo set(DOTNET_CORE_PATH "%escaped_loc%/runtimes/dotnet/shared/Microsoft.NETCore.App/5.0.12")>> %loc%\core\cmake\FindCoreCLR.cmake
-echo set(CORECLR_INCLUDE_DIR "%escaped_loc%/runtimes/dotnet/include")>> %loc%\core\cmake\FindCoreCLR.cmake
-echo include(FindPackageHandleStandardArgs)>> %loc%\core\cmake\FindCoreCLR.cmake
-echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(CoreCLR REQUIRED_VARS DOTNET_CORE_PATH CORECLR_INCLUDE_DIR VERSION_VAR CoreCLR_VERSION)>> %loc%\core\cmake\FindCoreCLR.cmake
-echo mark_as_advanced(DOTNET_CORE_PATH CORECLR_INCLUDE_DIR)>> %loc%\core\cmake\FindCoreCLR.cmake
+echo set(CoreCLR_VERSION 5.0.12)> "%loc%\core\cmake\FindCoreCLR.cmake"
+echo set(DOTNET_CORE_PATH "%escaped_loc%/runtimes/dotnet/shared/Microsoft.NETCore.App/5.0.12")>> "%loc%\core\cmake\FindCoreCLR.cmake"
+echo set(CORECLR_INCLUDE_DIR "%escaped_loc%/runtimes/dotnet/include")>> "%loc%\core\cmake\FindCoreCLR.cmake"
+echo include(FindPackageHandleStandardArgs)>> "%loc%\core\cmake\FindCoreCLR.cmake"
+echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(CoreCLR REQUIRED_VARS DOTNET_CORE_PATH CORECLR_INCLUDE_DIR VERSION_VAR CoreCLR_VERSION)>> "%loc%\core\cmake\FindCoreCLR.cmake"
+echo mark_as_advanced(DOTNET_CORE_PATH CORECLR_INCLUDE_DIR)>> "%loc%\core\cmake\FindCoreCLR.cmake"
 
 rem Patch for FindDotNET.cmake
-echo set(DOTNET_VERSION 5.0.12)> %loc%\core\cmake\FindDotNET.cmake
-echo set(DOTNET_MIGRATE 1)>> %loc%\core\cmake\FindDotNET.cmake
-echo set(DOTNET_COMMAND "%escaped_loc%/runtimes/dotnet/dotnet.exe")>> %loc%\core\cmake\FindDotNET.cmake
-echo include(FindPackageHandleStandardArgs)>> %loc%\core\cmake\FindDotNET.cmake
-echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(DotNET REQUIRED_VARS DOTNET_COMMAND DOTNET_MIGRATE VERSION_VAR DOTNET_VERSION)>> %loc%\core\cmake\FindDotNET.cmake
-echo mark_as_advanced(DOTNET_COMMAND DOTNET_MIGRATE DOTNET_VERSION)>> %loc%\core\cmake\FindDotNET.cmake
+echo set(DOTNET_VERSION 5.0.12)> "%loc%\core\cmake\FindDotNET.cmake"
+echo set(DOTNET_MIGRATE 1)>> "%loc%\core\cmake\FindDotNET.cmake"
+echo set(DOTNET_COMMAND "%escaped_loc%/runtimes/dotnet/dotnet.exe")>> "%loc%\core\cmake\FindDotNET.cmake"
+echo include(FindPackageHandleStandardArgs)>> "%loc%\core\cmake\FindDotNET.cmake"
+echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(DotNET REQUIRED_VARS DOTNET_COMMAND DOTNET_MIGRATE VERSION_VAR DOTNET_VERSION)>> "%loc%\core\cmake\FindDotNET.cmake"
+echo mark_as_advanced(DOTNET_COMMAND DOTNET_MIGRATE DOTNET_VERSION)>> "%loc%\core\cmake\FindDotNET.cmake"
 
 rem Patch for FindNodeJS.cmake
-echo set(NodeJS_VERSION 14.18.2)> %loc%\core\cmake\FindNodeJS.cmake
-echo set(NodeJS_INCLUDE_DIRS "%escaped_loc%/runtimes/nodejs/include/node")>> %loc%\core\cmake\FindNodeJS.cmake
-echo set(NodeJS_LIBRARY "%escaped_loc%/runtimes/nodejs/lib/libnode.lib")>> %loc%\core\cmake\FindNodeJS.cmake
-echo set(NodeJS_EXECUTABLE "%escaped_loc%/runtimes/nodejs/node.exe")>> %loc%\core\cmake\FindNodeJS.cmake
-echo include(FindPackageHandleStandardArgs)>> %loc%\core\cmake\FindNodeJS.cmake
-echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(NodeJS REQUIRED_VARS NodeJS_INCLUDE_DIRS NodeJS_LIBRARY NodeJS_EXECUTABLE VERSION_VAR NodeJS_VERSION)>> %loc%\core\cmake\FindNodeJS.cmake
-echo mark_as_advanced(NodeJS_VERSION NodeJS_INCLUDE_DIRS NodeJS_LIBRARY NodeJS_EXECUTABLE)>> %loc%\core\cmake\FindNodeJS.cmake
+echo set(NodeJS_VERSION 14.18.2)> "%loc%\core\cmake\FindNodeJS.cmake"
+echo set(NodeJS_INCLUDE_DIRS "%escaped_loc%/runtimes/nodejs/include/node")>> "%loc%\core\cmake\FindNodeJS.cmake"
+echo set(NodeJS_LIBRARY "%escaped_loc%/runtimes/nodejs/lib/libnode.lib")>> "%loc%\core\cmake\FindNodeJS.cmake"
+echo set(NodeJS_EXECUTABLE "%escaped_loc%/runtimes/nodejs/node.exe")>> "%loc%\core\cmake\FindNodeJS.cmake"
+echo include(FindPackageHandleStandardArgs)>> "%loc%\core\cmake\FindNodeJS.cmake"
+echo FIND_PACKAGE_HANDLE_STANDARD_ARGS(NodeJS REQUIRED_VARS NodeJS_INCLUDE_DIRS NodeJS_LIBRARY NodeJS_EXECUTABLE VERSION_VAR NodeJS_VERSION)>> "%loc%\core\cmake\FindNodeJS.cmake"
+echo mark_as_advanced(NodeJS_VERSION NodeJS_INCLUDE_DIRS NodeJS_LIBRARY NodeJS_EXECUTABLE)>> "%loc%\core\cmake\FindNodeJS.cmake"
 
-mkdir %loc%\core\build
-cd %loc%\core\build
+mkdir "%loc%\core\build"
+cd "%loc%\core\build"
 
 rem Build MetaCall
 cmake -Wno-dev ^
@@ -178,22 +178,18 @@ echo MetaCall Built Successfully
 
 rem Delete unnecesary data from tarball directory
 cd %loc%
-rmdir /S /Q %loc%\core
-rmdir /S /Q %loc%\dependencies
-rmdir /S /Q %loc%\nasm-2.15.05
-rmdir /S /Q %loc%\runtimes\dotnet\include
+rmdir /S /Q "%loc%\core"
+rmdir /S /Q "%loc%\dependencies"
+rmdir /S /Q "%loc%\nasm-2.15.05"
+rmdir /S /Q "%loc%\runtimes\dotnet\include"
+rmdir /S /Q "%loc%\runtimes\python\Lib\test"
 
-rem Delete runtime executables
-del /F /Q %loc%\runtimes\dotnet\dotnet.exe
-del /F /Q %loc%\runtimes\nodejs\node.exe
-del /F /Q %loc%\runtimes\ruby\bin\ruby.exe
-del /F /Q %loc%\runtimes\ruby\bin\rubyw.exe
-del /F /Q %loc%\runtimes\python\python.exe
-del /F /Q %loc%\runtimes\python\pythonw.exe
+rem Patch the C# Loader configuration
+echo { }> "%loc%\configurations\cs_loader.json"
 
 rem Move library dependencies to the correct folders
-move /Y %loc%\runtimes\nodejs\lib\libnode.dll %loc%\lib
-move /Y %loc%\runtimes\ruby\bin\x64-vcruntime140-ruby310.dll %loc%\lib
+move /Y "%loc%\runtimes\nodejs\lib\libnode.dll" "%loc%\lib"
+move /Y "%loc%\runtimes\ruby\bin\x64-vcruntime140-ruby310.dll" "%loc%\lib"
 
 echo Compressing the Tarball
 cd %dest%
--- metacall.bat
+++ metacall.bat
@@ -7,18 +7,25 @@ rem MetaCall
 set "PATH=%PATH%;%loc%;%loc%\lib"
 rem Python
 set "PYTHONHOME=%loc%\runtimes\python"
-set "PATH=%PATH%;%loc%\runtimes\python"
+set "PATH=%PATH%;%loc%\runtimes\python;%loc%\runtimes\python\Scripts"
 rem NodeJS
-set "PATH=%PATH%;%loc%\lib\runtimes\nodejs\lib"
+set "PATH=%PATH%;%loc%\lib\runtimes\nodejs;%loc%\lib\runtimes\nodejs\lib"
 rem Ruby
 set "PATH=%PATH%;%loc%\lib\runtimes\ruby\bin;%loc%\lib\runtimes\ruby\bin\ruby_builtin_dlls"
 rem DotNet Core
-set "PATH=%PATH%;%loc%\lib\runtimes\nodejs\lib"
-rem set "escaped_loc=%loc:\=\\%"
-rem echo { "dotnet_root":"%escaped_loc%\\runtimes\\dotnet\\shared\\Microsoft.NETCore.App\\5.0.12", "dotnet_loader_assembly_path":"%escaped_loc%\\lib\\CSLoader.dll" }> "%loc%\configurations\cs_loader.json"
-echo { }> "%loc%\configurations\cs_loader.json"
-set "CORE_ROOT=%loc%\runtimes\dotnet\shared\Microsoft.NETCore.App\5.0.12"
+set "PATH=%PATH%;%loc%\lib\runtimes\dotnet;%loc%\lib\runtimes\dotnet\host\fxr\5.0.12"
+
+rem Check if it is running a package manager (or related binary) and execute it
+if not [%1]==[] (
+	where /q "%1"
+	if %errorlevel% EQU 0 (
+		"%1"
+		exit /b %errorlevel%
+	)
+)
+
 rem MetaCall Enviroment
+set "CORE_ROOT=%loc%\runtimes\dotnet\shared\Microsoft.NETCore.App\5.0.12"
 set "LOADER_LIBRARY_PATH=%loc%\lib"
 set "SERIAL_LIBRARY_PATH=%loc%\lib"
 set "DETOUR_LIBRARY_PATH=%loc%\lib"
@@ -27,5 +34,7 @@ set "CONFIGURATION_PATH=%loc%\configurations\global.json"
 if not defined LOADER_SCRIPT_PATH (
 	set "LOADER_SCRIPT_PATH=%cd%"
 )
+
+rem Execute MetaCall CLI
 "%loc%\metacallcli.exe" %*
 endlocal
